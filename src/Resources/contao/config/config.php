d1fd37626b39a78e148b5ac53f66f18b3a4c8ce3
Aethys256 <aethys256@users.noreply.github.com>
2018-02-07 14:31:12 +0100

    Initial config & ide_compat

---
+++ src/Resources/contao/config/config.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * This file is part of the ActuSF Relations bundle designed for Contao 4.
+ *
+ * Copyright (c) 2016-2018 Addictic
+ * @package    Config
+ * @link       https://www.addictic.fr
+ * @author     Vianney CHANOUX <vchanoux@addictic.fr>
+ * @author     Quentin GIRAUD <qgiraud@addictic.fr>
+ */
+
+
+/**
+ * Backend form fields
+ */
+$GLOBALS['BE_FFL']['tableLookupAsfRelations'] = 'TableLookupAsfRelationsWizard';
---
+++ src/Resources/contao/config/ide_compat.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * This file is part of the ActuSF Relations bundle designed for Contao 4.
+ *
+ * Copyright (c) 2016-2018 Addictic
+ * @package    Config
+ * @link       https://www.addictic.fr
+ * @author     Vianney CHANOUX <vchanoux@addictic.fr>
+ * @author     Quentin GIRAUD <qgiraud@addictic.fr>
+ */
+
+
+// This file is not used in Contao. Its only purpose is to make PHP IDEs like
+// Eclipse, Zend Studio or PHPStorm realize the class origins, since the dynamic
+// class aliasing we are using is a bit too complex for them to understand.
+
+namespace {
+    \define('TL_ROOT', __DIR__ . '/../../../../../');
+    \define('TL_ASSETS_URL', 'http://localhost/');
+    \define('TL_FILES_URL', 'http://localhost/');
+}
+
+namespace {
+    class TableLookupAsfRelationsWizard extends \Contao\TableLookupAsfRelationsWizard {}
+}
